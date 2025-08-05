0f557b83bf835f469e94f75016e3ccfb0dfa8d6b
linuskjk <paulinus.brueder@gmail.com>
2025-08-05 13:50:15 +0200

    Update send_2fa_code.php

--- SecureFile/send_2fa_code.php
+++ SecureFile/send_2fa_code.php
@@ -36,12 +36,12 @@ try {
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
-    $mail->Username = 'my2fasender@gmail.com';
-    $mail->Password = 'eavkxgvlgzpyhusa';
+    $mail->Username = '';
+    $mail->Password = '';
     $mail->SMTPSecure = 'tls';
     $mail->Port = 587;
 
-    $mail->setFrom('my2fasender@gmail.com', 'SecureFile');
+    $mail->setFrom('', 'SecureFile');
     $mail->addAddress($email, $username);
 
     $mail->Subject = "Your SecureFile 2FA Code";
@@ -52,4 +52,5 @@ try {
 } catch (Exception $e) {
     echo json_encode(['status' => 'error', 'error' => 'Failed to send email: ' . $mail->ErrorInfo]);
 }
+
 ?>
